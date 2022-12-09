d8bd822c998916ff83b5382c14f32be8b583fa7d
eitangrunseid <77745620+eitangrunseid@users.noreply.github.com>
2022-12-09 13:52:19 +0200

    for gidon

--- js/footer.js
+++ js/footer.js
@@ -1,15 +1,17 @@
-const footer = document.querySelector('.footer-holder');
+const footer = document.querySelector(".footer-holder");
 
-const name1 = 'HTML';
-const name2 = 'CSS';
-const name3 = 'JAVA-SCRIPT';
-const and = 'and';
+const name1 = "HTML";
+const name2 = "CSS";
+const name3 = "JAVA-SCRIPT";
+const and = "and";
 
-let lang = [name1,name2,name3];
+let lang = [name1, name2, name3];
 let result = [];
 
 for (let i = 0; i < lang.length; i++) {
-    var x = lang[i];
-    result.push(x);
-    footer.innerText = `this page was built using: ${result}` 
+	var x = lang[i];
+
+	if (i === lang.length - 1) result.push(and);
+	result.push(x);
 }
+footer.innerText = `this page was built using: ${result[0]}, ${result[1]}, ${result[2]} ${result[3]}`;
